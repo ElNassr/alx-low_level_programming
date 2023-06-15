0c84d2d1086db7ae80f373f36921351775705e56
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-15 14:38:02 -0700

    Commit message

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+/* printing text */
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
