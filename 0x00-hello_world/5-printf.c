00f7fe36a7168adb686f47030c6e1809a9cb761f
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-15 13:49:07 -0700

    Commit message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,12 @@
 /**
  * main - entry
  *
- * Return (0) on success
+ * Return 0 on success
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,/n");
+/* printing text */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+/* return 0 */	
 	return (0);
 }
