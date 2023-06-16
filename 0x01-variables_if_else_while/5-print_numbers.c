fba6a8ea7e9706771668f7bc3d73f6cbc07ee4c4
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-16 15:27:18 -0700

    Commit message

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
