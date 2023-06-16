908a68df27e4b8fcd46b72a7df664e179bbe5e5d
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-16 15:45:23 -0700

    Commit message

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(",");
+			putchar(" ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c~
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
