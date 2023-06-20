e60aaa7e0021c87766efa9046840b5c0746cd551
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-20 16:32:10 -0700

    Commit message

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_last_digit -  function
+* @n: number to print last digit
+*
+* Return: last digit
+*/
+int print_last_digit(int n)
+{
+	int i = 0;
+
+	i = n % 10;
+	return(i);
+}
