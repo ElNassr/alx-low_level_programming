44d3b0b8ba412cb7356c258f68cf3722298b2f53
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-20 15:19:48 -0700

    Commit message

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _islower -  function
+*
+* Return: 1 if lower, 0 otherwise
+*/
+int _islower(int c)
+{
+	int i = 97;
+	int j = 122;
+
+	for ((c >= i) && (c <= j))
+		return (1)
+	else
+		return (0)
+}
