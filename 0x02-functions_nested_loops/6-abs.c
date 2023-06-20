d2a39f33a1d0c18710394fa8f87612174f2fe49f
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-20 16:23:43 -0700

    Commit message

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _abs -  function
+* @n: number to check
+*
+* Return: No return in this function
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
