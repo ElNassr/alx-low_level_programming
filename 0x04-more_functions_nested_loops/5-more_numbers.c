e195e066495271ad48a3ba6002137b6a4ff281bd
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-22 12:27:09 -0700

    Commit message

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio>
+
+/**
+* more_numbers - print numbers 10 times
+* no arguments
+*
+* does not return anything
+*/
+void more_numbers(void)
+{
+	int i = 48;
+	int j = 49;
+	ink c = 0;
+	int k = 0;
+
+	while (c < 10)
+	{
+		i = 48;
+		k = 0;
+		while (k <= 14)
+		{
+			if (k > 10)
+			{
+				putchar(j);
+			}
+			putchar(i);
+			if (k == 9)
+			{
+				i = 48;
+			}
+			k++;
+			i++;
+		}
+		c++;
+	}
+}
