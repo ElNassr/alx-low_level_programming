6b23a7964bc95fa6e327a3755594f586eaad2dea
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-22 15:08:28 -0700

    Commit message

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,27 +4,32 @@
 /**
  * fizz_buzz - function
  *
- * No return
+ * Return:  0 on success
  */
 int main(void)
 {
 	int i = 1;
+
 	for (i = 1; i < 101; i++)
 	{
-		if ((i % 3) == 0)
+		if ((i % 15) == 0)
 		{
-			printf("Fizz ");
+			printf("FizzBuzz ");
 		}
 		else if ((i % 5) == 0)
 		{
 			if (i == 100)
+			{
 				printf("Buzz\n");
+			}
 			else
+			{
 				printf("Buzz ");
+			}
 		}
-		else if ((i % 15) == 0)
+		else if ((i % 3) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("Fizz ");
 		}
 		else
 		{
