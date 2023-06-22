d2b2c63f86fc93bdf993def9fdb2c4451660cb12
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-22 11:21:48 -0700

    Commit message

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit -  function
+ * @c: char input to chack
+ *
+ * Return: 1 if c is digit, 0 otherwise
+ */
+int _isupper(int c)
+{
+	int i = 48;
+	int j = 57;
+
+	if (c >= i)
+	{
+		if (c <= j)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
