15f115f7279645640650ae97ade2bf9029e20b92
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-22 14:04:55 -0700

    Commit message

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void print_diag_line(int j);
+
+/**
+* print_diagonal - print diagonal line
+* no arguments
+*
+* No return
+*/
+void print_diagonal(int n)
+{
+	int i = 92;
+	int j = 0;
+
+	if (n <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		while (j < n)
+		{
+			if (j == 0)
+			{
+				putchar(i);
+				putchar('\n');
+			}
+			else if (j <= (n - 1))
+			{
+				print_diag_line(j);
+				putchar('\n');
+			}
+			j++;
+		}
+	}
+}
+
+/**
+* print_diag_line - function
+* @j: argument
+*
+* No return
+*/
+void print_diag_line(int j)
+{
+	int k = 0;
+	int s = 32;
+	int i = 92;
+
+	while (k < j)
+	{
+		putchar(s);
+		k++;
+	}
+	putchar(i);
+}
