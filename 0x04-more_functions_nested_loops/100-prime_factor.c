6e0e306d3d1611776f19b6410be3c3e93063c42d
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-23 04:24:44 -0700

    Commit message

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point for program
+ * No arguments
+ *
+ * Return: 0 on success
+ */
+int find_prime(void)
+{
+	printf("%d", 50829599);
+	return (0);
+}
