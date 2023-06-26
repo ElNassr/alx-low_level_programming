933a2dc013197426c2ddaa421e055d4d379cda05
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 12:23:54 -0700

    Commit message

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_rev - printd a string in reverse
+ * @s: string to revert
+ *
+ * Return: No return for this function
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	while (i >= 0)
+	{
+		putchar(*(s + i));
+		i--;
+	}
+	putchar('\n');
+}
