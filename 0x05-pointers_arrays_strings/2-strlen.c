97a60a805a685708d59c01e6f2b9fb98d983de33
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 11:49:09 -0700

    Commit message

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _strlen - calculate the length of a string
+ * @s: the string to find len for
+ *
+ * Return: returns an integer
+ */
+int _strlen(char *s)
+{
+	return (sizeof(s));
+}
