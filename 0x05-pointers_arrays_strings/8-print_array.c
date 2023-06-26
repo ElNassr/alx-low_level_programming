1deaf7f8b2abeb6e75bb0466224842f4098db653
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 15:18:56 -0700

    Commit message

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,15 @@ void print_number(int n);
 void print_array(int *a, int n)
 {
 	int i = 0;
+	int c = 44;
+	int s = 32;
 
 	for (i = 0; i < n; i++)
 	{
 		print_number(*(a + i));
+		putchar(c);
+		putchar(s);
+
 	}
 	putchar('\n');
 }
