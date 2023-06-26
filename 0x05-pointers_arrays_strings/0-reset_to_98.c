020d4fed8c184f5c3faa6350c4105ccc75bf5368
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 11:35:59 -0700

    Commit message

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that sets the value of variable to 98
+ * @*n: pointer to the variable we'll set
+ *
+ * Return: no return for this function
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
