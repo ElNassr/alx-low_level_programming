6379489e2b231a07349f3309943ca11cce28a5cd
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 14:42:12 -0700

    Commit message

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * puts_half - function that print half of string
+ * @str: string to be printed by this function
+ *
+ * Return: no return for this function
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	for (j = (i / 2); j < i; j++)
+	{
+		putchar(*(str + j));
+	}
+	if ((i % 2) == 0)
+		putchar(*(str + i - 1));
+	putchar('\n');
+}
