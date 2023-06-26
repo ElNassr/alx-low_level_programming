0ddd0ee0a1daf47e29e99c1dd0ef79b81ae245b5
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-26 15:53:21 -0700

    Commit message

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * _atoi: function that converts string to integer
+ * @s: string that will be converted to integer
+ *
+ * retunr: returns an integer if exist, 0 otherwise
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int r = 0;
+
+	while (*(s + i) != '\0')
+		i++;
+	for (j = 0; j < i)
+	{
+		r = _isalpha(*(s + j));
+	}
+	return (r);
+}
+
+/**
+ * _isalpha -  function
+ * @c: char input to chack
+ *
+ * Return: 1 if alpha, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	int i = 48;
+	int j = 57;
+
+	if (c >= i)
+	{
+		if (c <= j)
+		{
+			return (c - 48);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	else if (c >= 65)
+	{
+		if (c <= 90)
+			return (c - 48);
+		else
+			return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
