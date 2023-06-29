1806bcdfd273d64549e8dbecffbc45547dda3cdc
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 12:56:38 -0700

    Commit message

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: return an integer depending on comparison
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (*(s1 + i) != '\0')
+	{
+		if (*(s1 + i) < *(s2 + i))
+			return (-15);
+		else if (*(s1 + i) > *(s2 + i))
+			return (15);
+	}
+	if (*(s1 + i) == *(s2 + i))
+		return (0);
+}
