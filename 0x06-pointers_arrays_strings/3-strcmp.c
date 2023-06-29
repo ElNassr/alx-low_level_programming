934f6cadab474b1b84a2ea945e3a0a52dd834118
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 13:19:37 -0700

    Commit message

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,17 +10,42 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+	int l = 0;
+	int k = 0;
 
-	while (*(s1 + i) != '\0')
+	while (*(s1 + l) != '\0')
 	{
-		if (*(s1 + i) < *(s2 + i))
-			return (-15);
-		else if (*(s1 + i) > *(s2 + i))
-			return (15);
-		else
-			i++;
+		l++;
+	}
+	while (*(s2 + k) != '\0')
+	{
+		k++;
+	}
+	if (l < k)
+	{
+		return (-15);
+	}
+	else if (l > k)
+	{
+		return (15);
+	}
+	else
+	{
+		for (i = 0; i < k; i++)
+		{
+			if (*(s1 + i) < *(s2 + i))
+			{
+				return (-15);
+			}
+			else if (*(s1 + i) > *(s2 + i))
+			{
+				return (15);
+			}
+		}
 	}
 	if (*(s1 + i) == *(s2 + i))
+	{
 		return (0);
-	return (-15);
+	}
+	return (0);
 }
