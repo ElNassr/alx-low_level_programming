acf2aff21985d732d75b923207930ea8d7ada33f
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 11:41:47 -0700

    Commit message

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,7 +22,7 @@ char *_strncat(char *dest, char *src, int n)
 		*(dest + i + c) = *(src + c);
 	}
 	if (*(dest + i + n - 1) != '\0')
- 		*(dest + i + n - 1) = '\0';
+ 		*(dest + i + n) = '\0';
 	return (dest);
 
 }
