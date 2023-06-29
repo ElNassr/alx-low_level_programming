cc22abac9f11c74b0c23bce64277824c086e99ce
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 11:25:08 -0700

    Commit message

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * _strncat - function that concatenates two strings
+ * @dest: variable for contenation destination
+ * @src: Variable for the string to concatenat in dest
+ * @n: number of bytes to concatenate
+ *
+ * Return: returns a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int c = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+	for (c = 0; c < n; c++)
+	{
+		*(dest + i + c) = *(src + c);
+	}
+	if (*(dest + i + n - 1) != '\0')
+		*(dest + i + n) = '\0';
+	return (dest);
+
+}
