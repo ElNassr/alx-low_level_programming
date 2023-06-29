40d31f595394d72e7883c4e6453d0ec7d5154c73
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 16:15:03 -0700

    Commit message

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * infinite_add - function to infinite add
+ * @n1: first n1 to add
+ * @n2: second n2 to add
+ * @r: r variable to do what it does
+ * @size_r: size of variable r
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	char *p = 0;
+	return (p);
+}
