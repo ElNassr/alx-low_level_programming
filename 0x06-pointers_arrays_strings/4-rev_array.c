22157cadedc34707ae0f641ac5a33c9b2f454fd1
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-06-29 13:47:18 -0700

    Commit message

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - function that reverses array
+ * @a: array to reverse
+ * @n: elements of the array
+ *
+ * Return: no return for this function
+ */
+void reverse_array(int *a, int n)
+{
+	int i = n;
+	int j = 0;
+	int t = 0;
+	int *p = a;
+
+	while (i >= j)
+	{
+		t = *(p + i);
+		*(p + i) = *(p + j);
+		*(p + j) = t;
+		i--;
+		j++;
+	}
+}
