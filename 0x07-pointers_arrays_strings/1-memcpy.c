5f046beca7883f42ffc2337da1d59c73fe41b7fd
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-03 11:47:52 -0700

    Commit message

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,7 +8,7 @@
  *
  * Return: returns a pointer to the memory changed
  */
-char *_memset(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
