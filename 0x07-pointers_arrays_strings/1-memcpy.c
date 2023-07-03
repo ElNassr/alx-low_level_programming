49b2edf40049db4285766d444bf091c34a3745da
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-03 11:44:50 -0700

    Commit message

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - fills memory with constant byte
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: the number of bytes to copy from memory
+ *
+ * Return: returns a pointer to the memory changed
+ */
+char *_memset(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
