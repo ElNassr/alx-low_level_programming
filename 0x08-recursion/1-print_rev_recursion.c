58ee657576d21b63b42a069a520f1cad146e41b6
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-05 14:41:24 -0700

    Commit message

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,14 +13,6 @@ void _print_rev_recursion(char *s)
 	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
-		if (*s != '\n')
-		{
-			putchar(*s);
-		}
-	}
-	else
-	{
-		putchar('\n');
-
+		putchar(*s);
 	}
 }
