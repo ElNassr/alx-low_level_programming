20d81e4a5d71f6ea6f42f89c0e85359f83fd6326
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-05 11:23:01 -0700

    Commit message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,10 +10,10 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	if (*s != '\0')
 	{
-		putchar(*(s + i));
-		i++;
+		putchar(*(s));
+		_puts_recursion(s + 1);
 	}
 	putchar('\n');
 }
