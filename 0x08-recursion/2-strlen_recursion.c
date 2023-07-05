38ee4a427108f2a963666b2f43487259f84f3862
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-05 14:51:17 -0700

    Commit message

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to fin length for
+ *
+ * Return: length of a string as integer
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+	int *p = &i;
+
+	len(s, p);
+	return (i);
+}
+
+/**
+ * len - calculates the len for string
+ * @s: string to fin len dor
+ * @p: pointer to increment each time
+ *
+ *return: no return for this function
+ */
+void len(char *s, int *p)
+{
+	if (*s != '\0')
+	{
+		(*p)++;
+		len(s + 1);
+	}
+
+}
