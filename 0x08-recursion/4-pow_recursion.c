e0b89cee051f3ae72c7106d72f939e041bdd5fb9
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-05 15:05:13 -0700

    Commit message

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * _pow_recursion - raises x to the power of y
+ * @x: the number to be raised to power
+ * @y: the power to be raised to
+ *
+ * Return: am integer as power of x to y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 1)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * _pow_recursion(y - 1));
+	}
+}
---
+++ 0x08-recursion/4-pow_recursion.c~
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * factorial - calculates the factorial of a number
+ * @n: number to calculate factorial for
+ *
+ * Return: return the factorial of n
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
