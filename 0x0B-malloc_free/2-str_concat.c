4c6136c4dab64c643fbaeb3f613c04c2982d274f
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-10 13:09:58 -0700

    Commit message

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: a pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char *p;
+
+	while (*(s1 + i) != '\0')
+	{
+		i++;
+	}
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+	}
+	p = malloc(sizeof(char) * (i + j + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (k = 0; k < i; k++)
+	{
+		*(p + k) = *(s1 + k);
+	}
+	for (k = 0; k < j; k++)
+	{
+		*(p + i + k) = *(s2 + k);
+	}
+	return (p);
+}
