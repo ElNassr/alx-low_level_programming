dbe4489170a6d2d9f07306eb40b4a71e2f351ff6
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-11 10:26:32 -0700

    Commit message

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments
+ * @ac: number of arguments
+ * @av: array to contains the arguments
+ *
+ * Return: returnsa pointer to the concatenated arguments
+ */
+char *argstostr(int ac, char **av)
+{
+	int i = 0;
+	int j = 0;
+	int c = 0;
+	char *p;
+
+	if ((ac == 0) || (av == NULL))
+	{
+		return (NULL);
+	}
+	for (i = 1; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			j++;
+			c++;
+		}
+	}
+	p = malloc(sizeof(char) * (c + ac));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	c = 0;
+	for (i = 1; i < ac; i++)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			p[c] = av[i][j];
+			j++;
+			c++;
+		}
+		p[c] = av[i][j];
+		p[c + 1] = '\n';
+		c += 2;
+	}
+	return (p);
+}
