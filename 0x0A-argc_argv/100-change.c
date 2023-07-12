7b5583bea71d01a012a98da9c2a8d0424d02c5ed
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-12 16:02:46 -0700

    Commit message

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry point
+ * @argc: number of argument given to the program
+ * @argv: poiner to the list of arguments
+ *
+ * Return: Returns the number of arguments
+ */
+int main(int argc, char *argv[])
+{
+	int n, a, b, c, d = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	n = atoi(argv[1]);
+	a = n / 25;
+	n %= 25;
+	b = n / 10;
+	n %= 10;
+	c = n / 5;
+	n %= 5;
+	d = n / 2;
+	n %= 2;
+	printf("%d\n", (a + b + c + d + n));
+	return (0);
+}
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - program entry point
  * @argc: number of argument given to the program
  * @argv: poiner to the list of arguments
  *
- * Return: Returns the number of arguments
+ * Return: 0 if successful, 1 otherwise
  */
 int main(int argc, char *argv[])
 {
-	int n, a, b, c, d = 0;
-
-	if (argc != 2)
+	int i, j, k, sum = 0;
+	if (argc == 1)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
 	}
-	n = atoi(argv[1]);
-	a = n / 25;
-	n %= 25;
-	b = n / 10;
-	n %= 10;
-	c = n / 5;
-	n %= 5;
-	d = n / 2;
-	n %= 2;
-	printf("%d\n", (a + b + c + d + n));
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		k = 0;
+		while (argv[i][k] != '\0')
+		{
+			k++;
+		}
+		for (j = 0; j < k; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
 	return (0);
 }
