20238156daf174ce38030df8b09bbdeaa4a51df3
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-12 14:36:27 -0700

    Commit message

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program entry point
+ * @argc: number of argument given to the program
+ * @argv: poiner to the list of arguments
+ *
+ * Return: No return for this function
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return(argc);
+}
