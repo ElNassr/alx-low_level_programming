4d471b5a3110616c172455d12bb852a03b7e451d
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-13 11:50:34 -0700

    Commit message

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		j = n;
 	}
-	p = malloc(sizeof(char) * (i + j + 1));
+	p = malloc(sizeof(char) * (i + j));
 	if (p == NULL)
 	{
 		return (NULL);
