d350883f5b9e9799d5d17d4ef85e4e84114a4ef5
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-17 14:41:49 -0700

    This is commit message

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,40 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - print struct dog
+ * @d: structure to print
+ *
+ * Return: no return for this function
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf ("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf ("Name: (nil)\n");
+		}
+		if (d->age != 0)
+		{
+			printf ("Name: %f\n", d->age);
+		}
+		else
+		{
+			printf("Name: (nil)\n");
+		}
+		if (d->owner != NULL)
+		{
+			printf ("Name: %s\n", d->owner);
+		}
+		else
+		{
+			printf ("Name: (nil)\n");
+		}
+	}
+}
