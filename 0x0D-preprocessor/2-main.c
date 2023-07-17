22ce3188d6c396f3c5129049888177e7069b0709
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-17 12:05:16 -0700

    This is commit message

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
+/**
  * main - entry point
- * 
+ *
+ * Return: returns 0 on success 
  */
-void main()
+int main()
 {
 	printf("__BASE_FILE__");
+	return (0);
 }
