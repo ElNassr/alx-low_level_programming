67d522b8a8810a656e783a8b678aab604d93dc84
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-17 14:58:41 -0700

    This is commit message

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,23 +15,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_dog->name = malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
-		return NULL;
+		return (NULL);
 	}
 	strcpy(new_dog->name, name);
 	strcpy(new_dog->owner, owner);
 	new_dog->age = age;
-	return new_dog;
+	return (new_dog);
 }
