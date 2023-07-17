3590f3cab5f3fc13aa9ed38693ef5ac77a6c31ba
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-17 14:22:41 -0700

    This is commit message

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
 
 /**
  * init_dog - initialise a dog
@@ -12,7 +13,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = *name;
-	d.age = age;
-	d->owner = *owner;
+	if ( d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+
+	}
 }
