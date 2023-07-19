6ee5ec1b67ab9fbae488e0de7cdc694f96233175
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-19 14:18:02 -0700

    This is commit message

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: pointer to function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+
+}
