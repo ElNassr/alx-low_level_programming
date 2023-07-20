045d6e40b108ca02126b8c3ad53d18098f1494aa
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-20 16:43:55 -0700

    Commit message

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - prints anything
+ * @format: types of arg passed to the function
+ *
+ * Return: Void
+ */
+void print_all(const char * const format, ...)
+{
+	va_list p;
+	unsigned i = 0;
+	char *s;
+
+	va_start(p, format);
+	while (format && format[i])
+	{
+	case 'c':
+		printf("%c", va_arg(p, int));
+		break;
+	case 'i':
+		printf("%d", va_arg(p, int));
+		break;
+	case 's':
+		s = va_arg(p, char *);
+		if (!s)
+			s = "(nil)";
+		printf("%s", s);
+		break;
+	default:
+		i++;
+		continue;
+	}
+	if (format[i + 1])
+		printf(", ");
+	i++;
+}
