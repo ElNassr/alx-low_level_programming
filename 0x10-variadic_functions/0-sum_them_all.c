3e7d9fb102036c7c41c6d1ba9ef868af5cd109cb
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-20 15:43:17 -0700

    Commit message

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+/**
+ * sum_them_all - variadic function to summ
+ *                all parameters
+ * @n: number of parameters
+ *
+ * Retunr: the sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list p;
+	unsigned int i;
+	int sum = 0;
+
+	va_start (p, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg (p, int);
+	}
+	va_end (p);
+	return (sum);
+}
