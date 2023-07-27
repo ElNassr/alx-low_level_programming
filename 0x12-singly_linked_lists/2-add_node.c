265680c170b89e1555351e48f144e550d13ef73d
EL-YAMANI Nassr <elyamani.nasr@gmail.com>
2023-07-27 15:38:58 -0700

    Commit message

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a node in the beginning of list
+ * @head: pointer to the beginning of the list
+ * @str: string of the new list
+ *
+ * Return: address of the new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	new = (list_t *)malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
